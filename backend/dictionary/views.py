d29dd10c5e2b8df50e9fe993c554f6f16b423956
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-11-16 16:07:14 +0100

    upd

--- backend/dictionary/views.py
+++ backend/dictionary/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def hello(request):
-    return HttpResponse('hello world. We are using dockerrrr!!!!')
+    return HttpResponse('hello world. using dockerrrr!!!!')
+
+
 
 
 #  npx tailwindcss -i ./src/style.css -o ./dist/stylesheet.css --watch
 # docker-compose up
 # docker-compose exec web python manage.py runserver 0.0.0.0:8000
+
+# a match prediction system. on frontend, an input output system. a list of match is displayed served by backend. choose a match and it gives two possible outcomes
