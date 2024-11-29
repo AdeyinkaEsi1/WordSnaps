1059c47c3caf6bbe71596797c83be589124dea11
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-11-29 09:50:38 +0100

    fetch update

--- frontend/dist/index.js
+++ frontend/dist/index.js
@@ -1,30 +1,62 @@
-const predictor = {
-
+const dictionary = {
     init() {
-        this.predict();
+        this.define();
     },
 
-    predict() {
-        const outcomes = ['Apple', 'Banana', 'Cherry', 'Date'];
-        const pred_button = document.getElementById('predict-button')
+    define() {
+        document.getElementById('get-definition').addEventListener('click', () => {
+            const word = document.getElementById('word-input').value;
 
-        if (pred_button) {
-            pred_button.addEventListener('click', (event) => {
-                event.preventDefault();
-                const match = document.getElementById('match').value;
-                if (match) {
-                    const foutcome = outcomes[Math.floor(Math.random() * outcomes.length)];
-                    console.log(foutcome);
-                    const outcomeDiv = document.querySelector('.bg-white');
-                    if (outcomeDiv) {
-                        outcomeDiv.textContent = `Outcome: ${foutcome}`;
+            fetch(`http://localhost:8000/define/${word}/`)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.error) {
+                        document.getElementById('definition').innerHTML = data.error
+                    } else {
+                        document.getElementById('definition').innerHTML = `
+                        <h2>${data.hwi.hw}</h2>
+                        <p>${data.shortdef[0]}</p>
+                        `;
                     }
-                }
-            })
-        }
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    document.getElementById('definition').innerHTML = 'Error Fetching Definition.';
+                });
+        })
     }
 
 }
 
-predictor.init()
+dictionary.init()
+
+// const predictor = {
+
+//     init() {
+//         this.predict();
+//     },
+
+//     predict() {
+//         const outcomes = ['Apple', 'Banana', 'Cherry', 'Date'];
+//         const pred_button = document.getElementById('predict-button')
+
+//         if (pred_button) {
+//             pred_button.addEventListener('click', (event) => {
+//                 event.preventDefault();
+//                 const match = document.getElementById('match').value;
+//                 if (match) {
+//                     const foutcome = outcomes[Math.floor(Math.random() * outcomes.length)];
+//                     console.log(foutcome);
+//                     const outcomeDiv = document.querySelector('.bg-white');
+//                     if (outcomeDiv) {
+//                         outcomeDiv.textContent = `Outcome: ${foutcome}`;
+//                     }
+//                 }
+//             })
+//         }
+//     }
+
+// }
+
+// predictor.init()
 
