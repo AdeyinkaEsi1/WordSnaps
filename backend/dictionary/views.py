8a35bc78e005c5f5c8173305429e22b16a7eb447
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-11-29 09:51:58 +0100

    define upd

--- backend/dictionary/views.py
+++ backend/dictionary/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import requests
+from django.http import JsonResponse
+from django.conf import settings
 
-def hello(request):
-    return HttpResponse('hello world. using dockerrrr!!!!')
+MERIAM_WEBSTER_API_KEY = '208aa66c-62ce-4125-a911-7df6158251ba'
+BASE_URL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/"
 
+
+def get_word_definition(request, word):
+    
+    url = f"{BASE_URL}{word}?key={MERIAM_WEBSTER_API_KEY}"
+    
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if data:
+            return JsonResponse(data[0], safe=False)
+        else:
+            return JsonResponse({'error': 'Word not found'}, status=404)
+    else:
+        return JsonResponse({'error': 'Failed to fetch word from Merriam Webster'}, status=500)    
+    
+    
+    
 
 
 
@@ -11,4 +29,3 @@ def hello(request):
 # docker-compose up
 # docker-compose exec web python manage.py runserver 0.0.0.0:8000
 
-# a match prediction system. on frontend, an input output system. a list of match is displayed served by backend. choose a match and it gives two possible outcomes
